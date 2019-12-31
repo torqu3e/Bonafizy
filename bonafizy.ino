7677b468801d6df5c347454d1ac2c6b8f276e8e0
teji <teji.git@torqu3e.net>
2019-12-31 11:37:46 -0500

    Setup OTA

--- bonafizy.ino
+++ bonafizy.ino
@@ -1,8 +1,16 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <WiFiClient.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 //------------------------------------------
+
+#ifndef STASSID
+#define STASSID "WIFI_SSID"
+#define STAPSK  "WIFI_PASSWORD"
+#endif
 
 #define POWER_BUTTON D1 //Pin attached to kettle's power button
 #define HOLD_BUTTON D2 //Pin attached to kettle's hold button
@@ -11,8 +19,8 @@
 
 //------------------------------------------
 //WIFI
-const char* ssid = "WIFI_SSID";
-const char* password = "WIFI_PASSWORD";
+const char* ssid = STASSID;
+const char* password = STAPSK;
 
 //------------------------------------------
 //HTTP
@@ -29,19 +37,60 @@ void setup() {
   pinMode (HOLD_LED, INPUT);
   
   Serial.begin(115200);
-
+  
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   Serial.println("");
 
+  uint8_t wifi_timer = 0;
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
+    if (wifi_timer >= 10) {
+      Serial.println("Wifi not connected, rebooting...");
+      ESP.reset();
+    }
+    wifi_timer++;
   }
+
   Serial.println("");
   Serial.print("Connected to: ");
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
 
   server.on("/", HTTP_GET, HandleRoot);
   server.on("/state", HTTP_GET, get_state);
@@ -145,4 +194,5 @@ void HandleNotFound(){
 
 void loop() {
   server.handleClient();
+  ArduinoOTA.handle();
 }
