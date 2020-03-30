d0789d0ad608e002414c5fa9484f0b4b05fca79c
Tejinder Singh <35347971+torqu3e@users.noreply.github.com>
2020-03-29 21:31:33 -0400

    mDNS (#4)
    * initial uP scratch
    
    * mDNS handler
    Sets up a .local domain instead of having to use an IP
    test script updates
    
    * Test script update and todo

--- test_endpoints.sh
+++ test_endpoints.sh
@@ -6,14 +6,20 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 RESULTS_FILE="test_0.1.1.log"
-KETTLE_IP="192.168.1.186"
+KETTLE_HOSTNAME="kettle.local"
+KETTLE_IP=$(ping -c 1 $KETTLE_HOSTNAME | grep icmp_seq | awk '{print $4}' | tr -d ':')
 API_ENDPOINTS="power/off hold/off hold/on power/on hold/off hold/on hold/on power/off brew brew power/off state"
+start_time=$(date +%s)
 
 date > $1
+echo "Kettle IP: $KETTLE_IP" >> $1
 
 for i in $API_ENDPOINTS; do 
-    curl -s http://$KETTLE_IP/$i; 
+    echo "curl -s http://$KETTLE_IP/$i";
+    curl -s http://$KETTLE_IP/$i | tee -a $1; 
     sleep 1; 
-done >> $1
+done
 
+end_time=$(date +%s)
+echo "Test time: $((end_time - start_time)) seconds" | tee -a $1
 diff $RESULTS_FILE $1
